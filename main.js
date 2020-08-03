6514323ce538b4c35d3fe920a52ba44a55aa4873
jeyowthreeshwa <joeshua.dequina@gmail.com>
2020-08-04 00:09:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 var num0 = 0;
 var num1 = 0;
+var num2 = 0;
 var allowScroll = true;
 
 const MAX_PAGES = 8; // change depending on number of pages
@@ -12,6 +13,66 @@ $('html, body').animate({
     duration: 500
 });
 
+function scrll_move(){
+  $("#arrow1").animate({
+    top: "82vh"
+  }, {
+      duration: 1000,
+  });
+  $("#arrow1").animate({
+    top: "80vh"
+  }, {
+    duration: 1000,
+  });
+}
+
+
+function chckgdlns(){
+  $('html, body').animate({
+    scrollTop: $("#4").offset().top
+  }, {
+    duration: 1000,
+    complete: function () {
+        allowScroll = true;
+    }
+  });  
+  $("#banaag").animate({left: "-=250px", opacity: "0"}, {
+    duration: 1000
+  });
+  $("#dot0").addClass('is-deselected');
+  $("#dot0").removeClass('is-selected');
+  $("#dot4").removeClass('is-deselected');
+  $("#dot4").addClass('is-selected');
+  num1 = 4;
+  $("#arrow1").animate({opacity: "0"}, {
+    duration: 500
+  });
+}
+
+function sbmt_e(){
+  $('html, body').animate({
+    scrollTop: $("#8").offset().top
+  }, {
+    duration: 1000,
+    complete: function () {
+        allowScroll = true;
+    }
+  });  
+  $("#banaag").animate({left: "-=250px", opacity: "0"}, {
+    duration: 500
+  });
+  $("#dot0").addClass('is-deselected');
+  $("#dot0").removeClass('is-selected');
+  $("#dot8").removeClass('is-deselected');
+  $("#dot8").addClass('is-selected');
+  num1 = 8;
+  $("#arrow1").animate({opacity: "0"}, {
+    duration: 500
+  });
+}
+
+
+$(`#dot${num1}`).addClass('is-selected');
 $(window).bind('mousewheel', function(event) {
     num0 = event.originalEvent.wheelDelta/120;
     if (allowScroll) {
@@ -19,9 +80,13 @@ $(window).bind('mousewheel', function(event) {
         if (num0 < 0) {
             // downscroll code
             num1++;
+            num2 = 1;
             if (num1 === 1) {
                 $("#banaag").animate({left: "-=250px", opacity: "0"}, {
                     duration: 1000
+                });
+                $("#arrow1").animate({opacity: "0"}, {
+                  duration: 500
                 });
             }
             if (num1 > MAX_PAGES) {
@@ -30,9 +95,13 @@ $(window).bind('mousewheel', function(event) {
         } else if (num0 > 0) {
             // upscroll code
             num1--;
+            num2 = -1;
             if (num1 === 0) {
                 $("#banaag").animate({left: "+=250px", opacity: "1"}, {
-                    duration: 700
+                    duration: 1000
+                });
+                $("#arrow1").animate({opacity: "1"}, {
+                  duration: 500
                 });
             }
             if (num1 < 0){
@@ -47,6 +116,13 @@ $(window).bind('mousewheel', function(event) {
                 allowScroll = true;
             }
         });
+        
+        $(`#dot${num1-num2}`).addClass('is-deselected');
+        $(`#dot${num1-num2}`).removeClass('is-selected');
+        $(`#dot${num1}`).removeClass('is-deselected');
+        $(`#dot${num1}`).addClass('is-selected');
     }
 });
-    
+
+
+       
