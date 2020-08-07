7c3ec80700c3ff9a7f99baf1c875524d4de8bc5c
sonroyaalmerol <sonroya.almerol@gmail.com>
2020-08-07 22:02:02 +0800

    clean the fuck out of this js

--- main.js
+++ main.js
@@ -1,476 +1,184 @@
 
-var num0 = 0;
-var num1 = 0;
-var num2 = 0;
+var currentPage = -1;
+
 var num_l = 0;
 var num_l_prev = 0;
 var l_width = 3.3;
+
 var allowScroll = true;
+var banaagpresent = true;
 
 const MAX_PAGES = 8; // change depending on number of pages
 
-// always reset to #0
-$('html, body').animate({
-    scrollTop: $("#0").offset().top
-}, {
-    duration: 1000
-});
 
-var banaagpresent = true;
-
-function scrll_move(){
-  $(".scrll_arrw").animate({
+function scrll_move() {
+  $(".scrll_arrw").stop().animate({
     top: "82vh"
   }, {
       duration: 1000,
   });
-  $(".scrll_arrw").animate({
+  $(".scrll_arrw").stop().animate({
     top: "80vh"
   }, {
     duration: 1000,
   });
 }
 
+function nav_selection_update() {
+  num_l_prev = num_l;
+  if(currentPage < 1){
+    num_l = 0;
+    l_width = 3.3;
+  }
+  else if(currentPage > 0 && currentPage < 3){
+    num_l = 1;
+    l_width = 3.5;
+  }
+  else if(currentPage == 3){
+    num_l = 2;
+    l_width = 3.6;
+  }
+  else if (currentPage > 3 && currentPage < 8){
+    num_l = 3;
+    l_width = 6.6;
+  }
+  else if(currentPage == 8){
+    num_l = 4;
+    l_width = 4.2;
+  }
 
+  if (Math.abs(num_l-num_l_prev) != 0) { 
+    $(`#line${num_l}`).stop().animate({width: `${l_width}em`}, {
+      duration: 1000
+    });
+    $(`#line${num_l_prev}`).stop().animate({width: "0em"}, {
+      duration: 1000
+    });
+    $(`#mobile${num_l_prev}`).addClass('mobile_deselected');
+    $(`#mobile${num_l_prev}`).removeClass('mobile_selected');
+    $(`#mobile${num_l}`).removeClass('mobile_deselected');
+    $(`#mobile${num_l}`).addClass('mobile_selected');
+  } else {
+    $(`#line${num_l}`).stop().animate({width: `${l_width}em`}, {
+      duration: 1000
+    });
+  }
+}
 
-$(`#dot${num1}`).addClass('is-selected');
-$(`#mobile${num1}`).addClass('mobile_selected');
-document.getElementById("1").style.opacity = "0";
-document.getElementById("2").style.opacity = "0";
-document.getElementById("3").style.opacity = "0";
-document.getElementById("4").style.opacity = "0";
-document.getElementById("5").style.opacity = "0";
-document.getElementById("6").style.opacity = "0";
-document.getElementById("7").style.opacity = "0";
-document.getElementById("8").style.opacity = "0";
-$(`#line${num_l}`).animate({width: `${l_width}em`}, {
-  duration: 1000
-});
-$(window).bind('mousewheel', function(event) {
-    num0 = event.originalEvent.wheelDelta/120;
-    if (allowScroll) {
-        allowScroll = false;
-        if (num0 < 0) {
-            // downscroll code
-            downscroll_code();
-        } else if (num0 > 0) {
-            // upscroll code
-            upscroll_code();
-        }
-        $('html, body').animate({
-            scrollTop: $(`#${num1}`).offset().top
-        }, {
-            duration: 1000,
-            complete: function () {
-                allowScroll = true;
-            }
-        });
-        
-        $(`#${num1}`).animate({opacity: "1"}, {
-          duration: 1000
-        });
-        $(`#${num1-num2}`).animate({opacity: "0"}, {
-          duration: 400
-        });
-        
-        $(`#dot${num1-num2}`).addClass('is-deselected');
-        $(`#dot${num1-num2}`).removeClass('is-selected');
-        $(`#dot${num1}`).removeClass('is-deselected');
-        $(`#dot${num1}`).addClass('is-selected');
-        num_l_prev = num_l;
-        if(num1 < 1){
-          num_l = 0;
-          l_width = 3.3;
-        }
-        else if(num1 > 0 && num1 < 3){
-          num_l = 1;
-          l_width = 3.5;
-        }
-        else if(num1 == 3){
-          num_l = 2;
-          l_width = 3.6;
-        }
-        else if (num1 > 3 && num1 < 8){
-          num_l = 3;
-          l_width = 6.6;
-        }
-        else if(num1 == 8){
-          num_l = 4;
-          l_width = 4.2;
-        }
-        if(Math.abs(num_l-num_l_prev) != 0){ 
-          $(`#line${num_l}`).animate({width: `${l_width}em`}, {
-            duration: 1000
-          });
-          $(`#line${num_l_prev}`).animate({width: "0em"}, {
-            duration: 1000
-          });
-          $(`#mobile${num_l_prev}`).addClass('mobile_deselected');
-          $(`#mobile${num_l_prev}`).removeClass('mobile_selected');
-          $(`#mobile${num_l}`).removeClass('mobile_deselected');
-          $(`#mobile${num_l}`).addClass('mobile_selected');
-        }
-    }
-});
-
-function back_to_top(){
-  allowScroll = false;
-  $('html, body').animate({
-    scrollTop: $("#0").offset().top
-  }, {
-      duration: 1000,
-      complete: function () {
-          allowScroll = true;
-      }
+function change_dot_selection(select, deselect) {
+  $(`#${select}`).stop().animate({opacity: "1"}, {
+    duration: 1000
   });
-  $(`#${num1}`).animate({opacity: "0"}, {
+  $(`#${deselect}`).stop().animate({opacity: "0"}, {
     duration: 400
   });
-  num1 = 0;
-  $(`#${num1}`).animate({opacity: "1"}, {
-    duration: 1000
-  });
-  $("#dot8").addClass('is-deselected');
-  $("#dot8").removeClass('is-selected');
-  $("#dot0").removeClass('is-deselected');
-  $("#dot0").addClass('is-selected');
-  $("#banaag").animate({left: "+=250px", opacity: "1"}, {
-    duration: 1000
-  });
-  banaagpresent = true;
-  $(".scrll_arrw").animate({opacity: "1"}, {
-    duration: 1000
-  });
-  num_l_prev = 4;
-  num_l = 0;
-  l_width = 3.3;
-  $(`#line${num_l}`).animate({width: `${l_width}em`}, {
-    duration: 1000
-  });
-  $(`#line${num_l_prev}`).animate({width: "0em"}, {
-    duration: 1000
-  });
-  $("#mobile4").addClass('mobile_deselected');
-  $("#mobile4").removeClass('mobile_selected');
-  $("#mobile0").removeClass('mobile_deselected');
-  $("#mobile0").addClass('mobile_selected');
+
+  $(`#dot${deselect}`).addClass('is-deselected');
+  $(`#dot${deselect}`).removeClass('is-selected');
+  $(`#dot${select}`).removeClass('is-deselected');
+  $(`#dot${select}`).addClass('is-selected');
 }
 
-
-function dot_go_to(dot_num){
+function set_page(page) {
   allowScroll = false;
-  $('html, body').animate({
-    scrollTop: $(`#${dot_num}`).offset().top
-  }, {
-      duration: 1000,
-      complete: function () {
+
+  if (page > MAX_PAGES) {
+    allowScroll = true;
+    return;
+  }
+  else if (page < 0) {
+    allowScroll = true;
+    return;
+  }
+
+  if(page !== currentPage){
+    $('html, body').stop().animate({
+      scrollTop: $(`#${page}`).offset().top
+    }, {
+        duration: 1000,
+        complete: function () {
           allowScroll = true;
-      }
-  });
-  if(num1 != dot_num){
-    $(`#${num1}`).animate({opacity: "0"}, {
+        }
+    });
+    $(`#${currentPage}`).stop().animate({opacity: "0"}, {
       duration: 400
     });
-    $(`#${dot_num}`).animate({opacity: "1"}, {
+    $(`#${page}`).stop().animate({opacity: "1"}, {
       duration: 1000
     });
   }
-  if(dot_num === 0){
-    $(".scrll_arrw").animate({opacity: "1"}, {
+
+  if(page === 0) {
+    $(".scrll_arrw").stop().animate({opacity: "1"}, {
       duration: 1000
     });
-    if(banaagpresent == false){
-      $("#banaag").animate({left: "+=250px", opacity: "1"}, {
+    if(banaagpresent == false) {
+      $("#banaag").stop().animate({left: "+=250px", opacity: "1"}, {
         duration: 1000
       });
       banaagpresent = true;
     }
   }
-  else if(dot_num != 0){
-    $(".scrll_arrw").animate({opacity: "0"}, {
+  else if(page !== 0) {
+    $(".scrll_arrw").stop().animate({opacity: "0"}, {
       duration: 1000
     });
     if(banaagpresent == true){
-      $("#banaag").animate({left: "-=250px", opacity: "0"}, {
+      $("#banaag").stop().animate({left: "-=250px", opacity: "0"}, {
         duration: 1000
       });
       banaagpresent = false;
     }
   }
-  $(`#dot${num1}`).addClass('is-deselected');
-  $(`#dot${num1}`).removeClass('is-selected');
-  $(`#dot${dot_num}`).removeClass('is-deselected');
-  $(`#dot${dot_num}`).addClass('is-selected');
-  num1 = dot_num;
-  num_l_prev = num_l;
-  if(num1 < 1){
-    num_l = 0;
-    l_width = 3.3;
-  }
-  else if(num1 > 0 && num1 < 3){
-    num_l = 1;
-    l_width = 3.5;
-  }
-  else if(num1 == 3){
-    num_l = 2;
-    l_width = 3.6;
-  }
-  else if (num1 > 3 && num1 < 8){
-    num_l = 3;
-    l_width = 6.6;
-  }
-  else{
-    num_l = 4;
-    l_width = 4.2;
-  }
-  if(Math.abs(num_l-num_l_prev) != 0){ 
-    $(`#line${num_l}`).animate({width: `${l_width}em`}, {
-      duration: 1000
-    });
-    $(`#line${num_l_prev}`).animate({width: "0em"}, {
-      duration: 1000
-    });
-    $(`#mobile${num_l_prev}`).addClass('mobile_deselected');
-    $(`#mobile${num_l_prev}`).removeClass('mobile_selected');
-    $(`#mobile${num_l}`).removeClass('mobile_deselected');
-    $(`#mobile${num_l}`).addClass('mobile_selected');
-  }
-}
 
-
-function nav_go_to(num){
-  allowScroll = false;
-  $('html, body').animate({
-    scrollTop: $(`#${num}`).offset().top
-  }, {
-      duration: 1000,
-      complete: function () {
-          allowScroll = true;
-      }
-  }); 
-  if(num != num1){
-    $(`#${num1}`).animate({opacity: "0"}, {
-      duration: 400
-    });
-    $(`#${num}`).animate({opacity: "1"}, {
-      duration: 1000
-    });
-  }
+  change_dot_selection(page, currentPage);
+  currentPage = page;
   
-  if(num === 0){
-    $(".scrll_arrw").animate({opacity: "1"}, {
-      duration: 1000
-    });
-    if(banaagpresent == false){
-      $("#banaag").animate({left: "+=250px", opacity: "1"}, {
-        duration: 1000
-      });
-      banaagpresent = true;
-    }
-  }
-  else if(num != 0){
-    $(".scrll_arrw").animate({opacity: "0"}, {
-      duration: 1000
-    });
-    if(banaagpresent == true){
-      $("#banaag").animate({left: "-=250px", opacity: "0"}, {
-        duration: 1000
-      });
-      banaagpresent = false;
-    }
-  }
-  $(`#dot${num1}`).addClass('is-deselected');
-  $(`#dot${num1}`).removeClass('is-selected');
-  $(`#dot${num}`).removeClass('is-deselected');
-  $(`#dot${num}`).addClass('is-selected');
-  num1 = num;
-  num_l_prev = num_l;
-  if(num1 < 1){
-    num_l = 0;
-    l_width = 3.3;
-  }
-  else if(num1 > 0 && num1 < 3){
-    num_l = 1;
-    l_width = 3.5;
-  }
-  else if(num1 == 3){
-    num_l = 2;
-    l_width = 3.6;
-  }
-  else if (num1 > 3 && num1 < 8){
-    num_l = 3;
-    l_width = 6.6;
-  }
-  else{
-    num_l = 4;
-    l_width = 4.2;
-  }
-  if(Math.abs(num_l-num_l_prev) != 0){ 
-    $(`#line${num_l}`).animate({width: `${l_width}em`}, {
-      duration: 1000
-    });
-    $(`#line${num_l_prev}`).animate({width: "0em"}, {
-      duration: 1000
-    });
-    $(`#mobile${num_l_prev}`).addClass('mobile_deselected');
-    $(`#mobile${num_l_prev}`).removeClass('mobile_selected');
-    $(`#mobile${num_l}`).removeClass('mobile_deselected');
-    $(`#mobile${num_l}`).addClass('mobile_selected');
-  }
+  nav_selection_update();
 }
 
-       
-/*
-var rect = element.getBoundingClientRect();
-console.log(rect.top, rect.right, rect.bottom, rect.left);
-var bodyRect = document.body.getBoundingClientRect(),
-    elemRect = element.getBoundingClientRect(),
-    offset   = elemRect.top - bodyRect.top;
-
-alert('Element is ' + offset + ' vertical pixels from <body>');
-
-
-var left1 = document.getElementById("left1");
-left1.style.position = "fixed":
-left1.style.position = "absolute";
-*/
-
-/* 
-//-------set "Guidelines" to position:fixed----------
-//https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
-//https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element-relative-to-the-browser-window
-//http://jsbin.com/wigaca/1/edit?html,console,output
-function fix_gdlns(){
-  var elemRect = document.html.getBoundingClientRect(),
-      offsetTop   = elemRect.top - 20,
-      offsetLeft   = elemRect.left;
-
-  document.getElementById("bdy-gdlns1").style.position = "fixed";
-  console.log('done fixed');
-  document.getElementById("bdy-gdlns1").style.top = `${offsetTop}px`;
-  console.log(offsetTop);
-  document.getElementById("bdy-gdlns1").style.left = `${offsetLeft}px`;
-  console.log(offsetLeft);
-}
-*/
 
 $(document).ready(function() {
+  // mobile nav
   $(".button1").click(function() {
     $(".mobile_nav").toggleClass("active");
     $(".button1").toggleClass("btn");
   });
-});
 
+  // always reset to #0
+  set_page(0);
 
+  $(window).bind('mousewheel', function(event) {
+    var num0 = event.originalEvent.wheelDelta/120;
+    if (allowScroll) {
+      if (num0 < 0) {
+        // downscroll code
+        set_page(currentPage+1);
+      } else if (num0 > 0) {
+        // upscroll code
+        set_page(currentPage-1);
+      }
+    }
+  });
 
-function downscroll_code(){
-  num1++;
-  num2 = 1;
-  if (num1 === 1) {
-      $("#banaag").animate({left: "-=250px", opacity: "0"}, {
-          duration: 1000
-      });
-      banaagpresent = false;
-  }
-  if (num1 > 0) {
-    $(".scrll_arrw").animate({opacity: "0"}, {
-      duration: 500
-    });
-  }
+  let touchstartY = 0;
+  let touchendY = 0;
 
-  if (num1 > MAX_PAGES) {
-      num1 = MAX_PAGES;
-  }
-}
-function upscroll_code(){
-  num1--;
-  num2 = -1;
-  if (num1 === 0) {
-      $("#banaag").animate({left: "+=250px", opacity: "1"}, {
-          duration: 1000
-      });
-      banaagpresent = true;
-      $(".scrll_arrw").animate({opacity: "1"}, {
-        duration: 1000
-      });
-  }
-  if (num1 < 0){
-      num1 = 0;
-  }
-}
-let touchstartY = 0;
-let touchendY = 0;
-
-document.addEventListener('touchstart', function(event) {
+  document.addEventListener('touchstart', function(event) {
     touchstartY = event.changedTouches[0].screenY;
-}, false);
-document.addEventListener('touchend', function(event) {
+  }, false);
+  
+  document.addEventListener('touchend', function(event) {
     touchendY = event.changedTouches[0].screenY;
-    handleGesture();
-}, false); 
 
-function handleGesture() {
     if (allowScroll && touchendY != touchstartY) {
-      allowScroll = false;
       if (touchendY < touchstartY) {
-          // downscroll code
-        downscroll_code();
+        // downscroll code
+        set_page(currentPage+1);
       } else if (touchendY > touchstartY) {
-          // upscroll code
-        upscroll_code();
+        // upscroll code
+        set_page(currentPage-1);
       }
-      $('html, body').animate({
-          scrollTop: $(`#${num1}`).offset().top
-      }, {
-          duration: 1000,
-          complete: function () {
-              allowScroll = true;
-          }
-      });
-      
-      $(`#${num1}`).animate({opacity: "1"}, {
-        duration: 1000
-      });
-      $(`#${num1-num2}`).animate({opacity: "0"}, {
-        duration: 400
-      });
-      
-      $(`#dot${num1-num2}`).addClass('is-deselected');
-      $(`#dot${num1-num2}`).removeClass('is-selected');
-      $(`#dot${num1}`).removeClass('is-deselected');
-      $(`#dot${num1}`).addClass('is-selected');
-      num_l_prev = num_l;
-      if(num1 < 1){
-        num_l = 0;
-        l_width = 3.3;
-      }
-      else if(num1 > 0 && num1 < 3){
-        num_l = 1;
-        l_width = 3.5;
-      }
-      else if(num1 == 3){
-        num_l = 2;
-        l_width = 3.6;
-      }
-      else if (num1 > 3 && num1 < 8){
-        num_l = 3;
-        l_width = 6.6;
-      }
-      else if(num1 == 8){
-        num_l = 4;
-        l_width = 4.2;
-      }
-      if(Math.abs(num_l-num_l_prev) != 0){ 
-        $(`#line${num_l}`).animate({width: `${l_width}em`}, {
-          duration: 1000
-        });
-        $(`#line${num_l_prev}`).animate({width: "0em"}, {
-          duration: 1000
-        });
-        $(`#mobile${num_l_prev}`).addClass('mobile_deselected');
-        $(`#mobile${num_l_prev}`).removeClass('mobile_selected');
-        $(`#mobile${num_l}`).removeClass('mobile_deselected');
-        $(`#mobile${num_l}`).addClass('mobile_selected');
-      }
-  }
-}
+    }
+  }, false);
+});
