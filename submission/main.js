de15424372e2f40e392495893179a454f52f33fd
josephJaoNasser <57519561+josephJaoNasser@users.noreply.github.com>
2020-08-14 15:39:35 +0800

    Updated axios post link

--- submission/main.js
+++ submission/main.js
@@ -130,7 +130,7 @@ async function postToWP(data) {
   document.getElementById("form_submit").value = "Please wait...";
 
   // change url of this to actual rest api plugin
-  axios.post('http://atenewswp.lan/wp-json/atenews/v1/banaag_diwa_submit', data, {
+  axios.post('atenewswp.lan to be0320742902.ngrok.io', data, {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
